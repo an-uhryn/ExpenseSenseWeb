d21b027524e22ffed6fc71571d4325efab81aaaa
Andrii Uhryn <an.ugryn@gmail.com>
2024-02-18 19:43:23 +0100

    feat: implement categories

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,29 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+var cors = require('cors');
 
 dotenv.config();
+const port = process.env.PORT || 4000;
 
 const app: Express = express();
-const port = process.env.PORT || 4000;
+const mongoose = require('mongoose');
+const categoriesRoutes = require('./routes/categories')
+
+
+app.use(express.json())
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+app.use('/api/categories', categoriesRoutes);
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+}).catch((error: Error) => {
+  console.log(error)
+})
+
